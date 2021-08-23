23f1ba794a5e3d4a06d8c16762cee6ae4aae5ff3
Duncan Roosma <droosma@xpirit.com>
2021-08-23 23:12:01 +0200

    Add extensibility for Property filter and Collection hook

---
+++ src/TestServer.Configuration.InMemorySettings/PropertyInfoFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace Configuration.Extensions.InMemorySettings
+{
+    public class PropertyInfoFilter
+    {
+        private PropertyInfoFilter(BindingFlags bindingFlags,
+                                   Func<PropertyInfo, bool> propertyFilter)
+        {
+            BindingFlags = bindingFlags;
+            PropertyFilter = propertyFilter;
+        }
+
+        public BindingFlags BindingFlags { get; }
+        public Func<PropertyInfo, bool> PropertyFilter { get; }
+
+        public static PropertyInfoFilter Default
+            => new(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public,
+                   info => info.CanRead && info.CanWrite);
+
+        public static PropertyInfoFilter Create(BindingFlags bindingFlags,
+                                                Func<PropertyInfo, bool> propertyFilter)
+            => new(bindingFlags, propertyFilter);
+    }
+}
